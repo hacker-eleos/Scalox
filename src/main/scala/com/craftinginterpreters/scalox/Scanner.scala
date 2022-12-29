a44d0c12607725d440085997905d1ab3d466b3f9
Vikram Bhatt <vikrambhatt1405@gmail.com>
2022-12-30 01:34:42 +0530

    working condition

--- src/main/scala/com/craftinginterpreters/scalox/Scanner.scala
+++ src/main/scala/com/craftinginterpreters/scalox/Scanner.scala
@@ -1,14 +1,29 @@
 package com.craftinginterpreters.scalox
 
 class Scanner(source: String) {
-  def scanTokens(): List[Token] = {
-    val lines = source.linesIterator;
-    lines.zip(1 until lines.length).flatMap((line, i) => {
-      line.map {
-        case '(' => List(new Token(TokenType.LEFT_PAREN, "(", (), i ))
-        case _ => Scalox.error(i, "Unexpected string")
+  def scanTokens(): List[Token] | Unit = {
+    try {
+      val lines = source.linesIterator;
+      lines.zip(1 until lines.length).flatMap((line: String, i: Int) => {
+        line.map[Token] {
+          case '(' => Token(TokenType.LEFT_PAREN, "(", (), i)
+          case ')' => Token(TokenType.RIGHT_PAREN, ")", (), i)
+          case '{' => Token(TokenType.LEFT_BRACE, "{", (), i)
+          case '}' => Token(TokenType.RIGHT_BRACE, "}", (), i)
+          case ',' => Token(TokenType.COMMA, ",", (), i)
+          case '.' => Token(TokenType.DOT, ".", (), i)
+          case '-' => Token(TokenType.MINUS, ".", (), i)
+          case '+' => Token(TokenType.PLUS, ".", (), i)
+          case ';' => Token(TokenType.SEMICOLON, ".", (), i)
+          case '*' => Token(TokenType.STAR, ".", (), i)
+          case _ => throw Exception(s"line:${i}")
+        }
+      }).toList
+    } catch {
+      case e: Exception => {
+        val lineNumber = e.getMessage.split(":").apply(1).toInt
+        Scalox.error(lineNumber, "Unexpected string")
       }
-    })
-
+    }
   }
 }
--- src/main/scala/com/craftinginterpreters/scalox/Token.scala
+++ src/main/scala/com/craftinginterpreters/scalox/Token.scala
@@ -2,6 +2,6 @@ package com.craftinginterpreters.scalox
 
 import TokenType._
 
-class Token(final val tokenType: TokenType, final val lexeme: String, final val literal: AnyVal, final val line: Int) {
+case class Token(final val tokenType: TokenType, final val lexeme: String, final val literal: AnyVal, final val line: Int) {
   override def toString: String = tokenType.toString + " " + lexeme + " " + literal
 }
