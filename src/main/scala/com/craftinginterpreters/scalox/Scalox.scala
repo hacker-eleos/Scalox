0d23b6b74fdde1f32bb1a9e402c042b0da0001ac
Vikram Bhatt <vikrambhatt1405@gmail.com>
2022-12-30 22:57:03 +0530

    fix helper function

--- src/main/scala/com/craftinginterpreters/scalox/Scalox.scala
+++ src/main/scala/com/craftinginterpreters/scalox/Scalox.scala
@@ -2,35 +2,34 @@ package com.craftinginterpreters.scalox
 
 import java.nio.charset.Charset
 import scala.io.{Codec, Source, StdIn}
+import java.io.BufferedReader
 
 object Scalox {
 
   def main(args: Array[String]): Unit = {
     if (args.length > 1) {
       Console.println("Usage: scalox [script]")
-      sys.exit(64);
+      sys.exit(64)
     } else if (args.length == 1) {
-      runFile(args(0));
+      runFile(args(0))
     } else {
       runPrompt()
     }
   }
 
   def runFile(path: String): Unit = {
-    val bufferedReader = Source.fromFile(path)(new Codec(Charset.defaultCharset())).bufferedReader();
-    run(bufferedReader.toString);
-    bufferedReader.close();
+    val source = Source.fromFile(path)
+    run(try source.mkString finally source.close())
   }
 
   def runPrompt(): Unit = {
-    Iterator.continually(StdIn.readLine).takeWhile(_ != null).foreach(line => run(line));
+    Iterator.continually(StdIn.readLine).takeWhile(_ != null).foreach(line => run(line))
   }
 
   def run(source: String): Unit = {
-    Console.println(source)
-//    Scanner scanner = new Scanner(source)
-//    val tokens: List[Token] = scanner.scanTokens()
-//    tokens.foreach(Console.println)
+    val scanner = new Scanner(source)
+    val tokens: List[Token] = scanner.scanTokens()
+    tokens.foreach(Console.println)
   }
 
   def error(line: Int, message: String): Unit = {
@@ -40,7 +39,5 @@ object Scalox {
   def report(line: Int, where: String, message: String): Unit = {
     Console.err.println("[line " + line + "] Error" + where + ": " + message)
   }
-
-
 
 }
--- src/main/scala/com/craftinginterpreters/scalox/Scanner.scala
+++ src/main/scala/com/craftinginterpreters/scalox/Scanner.scala
@@ -1,48 +1,99 @@
 package com.craftinginterpreters.scalox
 
 import scala.annotation.tailrec
+import scala.collection.immutable.HashMap
 
-class Scanner(source: String) {
-  def scanTokens(): List[Token] | Unit = {
-    @tailrec
-    def scanTokens(currentIndex: Int, lineNumber: Int, collectedTokens: List[Token]): List[Token] | Unit = {
-      if (currentIndex >= source.length) return collectedTokens
+class Scanner(final val source: String) {
+  private def isDigit(c: Char) = c >= '0' && c <= '9'
 
-      def isMatchNextChar(char: Char): Boolean = {
-        if (currentIndex.equals(source.length - 1)) return false;
-        if (source(currentIndex).equals(char)) return true;
-        false
-      }
+  private def isAlphabet(c: Char) = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
 
-      val currentChar = source.apply(currentIndex);
-      currentChar match {
-        case '(' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.LEFT_PAREN, "(", (), lineNumber) :: collectedTokens)
-        case ')' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.RIGHT_PAREN, ")", (), lineNumber) :: collectedTokens)
-        case '{' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.LEFT_BRACE, "{", (), lineNumber) :: collectedTokens)
-        case '}' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.RIGHT_BRACE, "}", (), lineNumber) :: collectedTokens)
-        case ',' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.COMMA, ",", (), lineNumber) :: collectedTokens)
-        case '.' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.DOT, ".", (), lineNumber) :: collectedTokens)
-        case '-' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.MINUS, ".", (), lineNumber) :: collectedTokens)
-        case '+' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.PLUS, ".", (), lineNumber) :: collectedTokens)
-        case ';' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.SEMICOLON, ".", (), lineNumber) :: collectedTokens)
-        case '*' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.STAR, ".", (), lineNumber) :: collectedTokens)
-        case '!' =>
-          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.BANG_EQUAL, "!=", (), lineNumber) :: collectedTokens)
-          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.BANG, "!", (), lineNumber) :: collectedTokens)
-        case '=' =>
-          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.EQUAL_EQUAL, "==", (), lineNumber) :: collectedTokens)
-          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "=", (), lineNumber) :: collectedTokens)
-        case '<' =>
-          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.LESS_EQUAL, "<=", (), lineNumber) :: collectedTokens)
-          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "<", (), lineNumber) :: collectedTokens)
-        case '>' =>
-          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.GREATER_EQUAL, ">=", (), lineNumber) :: collectedTokens)
-          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "=", (), lineNumber) :: collectedTokens)
-        case '/' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.SLASH, "/", (), lineNumber) :: collectedTokens)
-        case _ => Scalox.error(lineNumber, "Unexpected string")
-      }
+  private val keywords = HashMap[String, TokenType](
+    ("and", TokenType.AND),
+    ("class", TokenType.CLASS),
+    ("else", TokenType.ELSE),
+    ("false", TokenType.FALSE),
+    ("for", TokenType.FOR),
+    ("fun", TokenType.FUN),
+    ("if", TokenType.IF),
+    ("nil", TokenType.NIL),
+    ("or", TokenType.OR),
+    ("print", TokenType.PRINT),
+    ("return", TokenType.RETURN),
+    ("super", TokenType.SUPER),
+    ("this", TokenType.THIS),
+    ("true", TokenType.TRUE),
+    ("var", TokenType.VAR),
+    ("while", TokenType.WHILE))
+
+  @tailrec
+  private def scanTokens(currentIndex: Int, lineNumber: Int, collectedTokens: List[Token]): List[Token] | Unit = {
+    if (currentIndex >= source.length) return collectedTokens
+
+    def isMatchNextChar(char: Char): Boolean = {
+      if (currentIndex.equals(source.length - 1)) return false
+      if (source(currentIndex).equals(char)) return true
+      false
     }
 
-    scanTokens(0, 0, Nil);
+    val currentChar = source.charAt(currentIndex)
+    currentChar match {
+      case '(' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.LEFT_PAREN, "(", null, lineNumber) :: collectedTokens)
+      case ')' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.RIGHT_PAREN, ")", null, lineNumber) :: collectedTokens)
+      case '{' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.LEFT_BRACE, "{", null, lineNumber) :: collectedTokens)
+      case '}' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.RIGHT_BRACE, "}", null, lineNumber) :: collectedTokens)
+      case ',' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.COMMA, ",", null, lineNumber) :: collectedTokens)
+      case '.' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.DOT, ".", null, lineNumber) :: collectedTokens)
+      case '-' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.MINUS, "-", null, lineNumber) :: collectedTokens)
+      case '+' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.PLUS, "+", null, lineNumber) :: collectedTokens)
+      case ';' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.SEMICOLON, ";", null, lineNumber) :: collectedTokens)
+      case '*' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.STAR, "*", null, lineNumber) :: collectedTokens)
+      case '!' =>
+        if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.BANG_EQUAL, "!=", null, lineNumber) :: collectedTokens)
+        else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.BANG, "!", null, lineNumber) :: collectedTokens)
+      case '=' =>
+        if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.EQUAL_EQUAL, "==", null, lineNumber) :: collectedTokens)
+        else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "=", null, lineNumber) :: collectedTokens)
+      case '<' =>
+        if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.LESS_EQUAL, "<=", null, lineNumber) :: collectedTokens)
+        else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "<", null, lineNumber) :: collectedTokens)
+      case '>' =>
+        if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.GREATER_EQUAL, ">=", null, lineNumber) :: collectedTokens)
+        else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "=", null, lineNumber) :: collectedTokens)
+      case '/' =>
+        if (isMatchNextChar('/'))
+          val endOfLineIndex = source.indexOf('\n', currentIndex)
+          if(endOfLineIndex == -1) Token(TokenType.COMMENT,"//", source.substring(currentIndex+2), lineNumber)::collectedTokens
+          else scanTokens(currentIndex+endOfLineIndex, lineNumber+1, Token(TokenType.COMMENT,"//", source.substring(currentIndex+2, endOfLineIndex), lineNumber)::collectedTokens)
+        else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.SLASH, "/", null, lineNumber) :: collectedTokens)
+
+      case ' ' => scanTokens(currentIndex + 1, lineNumber, collectedTokens)
+      case '\r' => scanTokens(currentIndex + 1, lineNumber, collectedTokens)
+      case '\t' => scanTokens(currentIndex + 1, lineNumber, collectedTokens)
+      case '\n' => scanTokens(currentIndex + 1, lineNumber + 1, collectedTokens)
+      case '"' =>
+        val endOfStringIndex = source.indexOf('"', currentIndex + 1)
+        if (endOfStringIndex == -1) Scalox.error(lineNumber, "Unterminated string")
+        val extractedString = source.substring(currentIndex + 1, endOfStringIndex)
+        val numberOfNewLines = extractedString.count(_ == '\n')
+        scanTokens(endOfStringIndex + 1, lineNumber + numberOfNewLines, Token(TokenType.STRING, extractedString, null, lineNumber) :: collectedTokens)
+      case c if isDigit(c) =>
+      case c if isAlphabet(c) =>
+        val identifier = source.substring(currentIndex).takeWhile(c => isAlphabet(c) || isDigit(c))
+        val tokenType = keywords.get(identifier)
+        tokenType match {
+          case Some(x) => scanTokens(currentIndex + identifier.length, lineNumber, Token(x, identifier, null, lineNumber) :: collectedTokens)
+          case None => scanTokens(currentIndex + identifier.length, lineNumber, Token(TokenType.IDENTIFIER, identifier, null, lineNumber) :: collectedTokens)
+        }
+      case _ => Scalox.error(lineNumber, "Unexpected string")
+    }
+  }
+
+  def scanTokens(): List[Token] = {
+    val tokens = scanTokens(0, 0, Nil)
+    tokens match {
+      case t: List[Token] => t
+      case _: Unit => Nil
+    }
   }
 }
