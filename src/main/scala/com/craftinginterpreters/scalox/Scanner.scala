105c8cc467decab04c718cc9b3840b18aeb666c6
Vikram Bhatt <vikrambhatt1405@gmail.com>
2022-12-30 10:34:46 +0530

    refactor scanner

--- src/main/scala/com/craftinginterpreters/scalox/Scanner.scala
+++ src/main/scala/com/craftinginterpreters/scalox/Scanner.scala
@@ -1,29 +1,48 @@
 package com.craftinginterpreters.scalox
 
+import scala.annotation.tailrec
+
 class Scanner(source: String) {
   def scanTokens(): List[Token] | Unit = {
-    try {
-      val lines = source.linesIterator;
-      lines.zip(1 until lines.length).flatMap((line: String, i: Int) => {
-        line.map[Token] {
-          case '(' => Token(TokenType.LEFT_PAREN, "(", (), i)
-          case ')' => Token(TokenType.RIGHT_PAREN, ")", (), i)
-          case '{' => Token(TokenType.LEFT_BRACE, "{", (), i)
-          case '}' => Token(TokenType.RIGHT_BRACE, "}", (), i)
-          case ',' => Token(TokenType.COMMA, ",", (), i)
-          case '.' => Token(TokenType.DOT, ".", (), i)
-          case '-' => Token(TokenType.MINUS, ".", (), i)
-          case '+' => Token(TokenType.PLUS, ".", (), i)
-          case ';' => Token(TokenType.SEMICOLON, ".", (), i)
-          case '*' => Token(TokenType.STAR, ".", (), i)
-          case _ => throw Exception(s"line:${i}")
-        }
-      }).toList
-    } catch {
-      case e: Exception => {
-        val lineNumber = e.getMessage.split(":").apply(1).toInt
-        Scalox.error(lineNumber, "Unexpected string")
+    @tailrec
+    def scanTokens(currentIndex: Int, lineNumber: Int, collectedTokens: List[Token]): List[Token] | Unit = {
+      if (currentIndex >= source.length) return collectedTokens
+
+      def isMatchNextChar(char: Char): Boolean = {
+        if (currentIndex.equals(source.length - 1)) return false;
+        if (source(currentIndex).equals(char)) return true;
+        false
+      }
+
+      val currentChar = source.apply(currentIndex);
+      currentChar match {
+        case '(' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.LEFT_PAREN, "(", (), lineNumber) :: collectedTokens)
+        case ')' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.RIGHT_PAREN, ")", (), lineNumber) :: collectedTokens)
+        case '{' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.LEFT_BRACE, "{", (), lineNumber) :: collectedTokens)
+        case '}' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.RIGHT_BRACE, "}", (), lineNumber) :: collectedTokens)
+        case ',' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.COMMA, ",", (), lineNumber) :: collectedTokens)
+        case '.' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.DOT, ".", (), lineNumber) :: collectedTokens)
+        case '-' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.MINUS, ".", (), lineNumber) :: collectedTokens)
+        case '+' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.PLUS, ".", (), lineNumber) :: collectedTokens)
+        case ';' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.SEMICOLON, ".", (), lineNumber) :: collectedTokens)
+        case '*' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.STAR, ".", (), lineNumber) :: collectedTokens)
+        case '!' =>
+          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.BANG_EQUAL, "!=", (), lineNumber) :: collectedTokens)
+          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.BANG, "!", (), lineNumber) :: collectedTokens)
+        case '=' =>
+          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.EQUAL_EQUAL, "==", (), lineNumber) :: collectedTokens)
+          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "=", (), lineNumber) :: collectedTokens)
+        case '<' =>
+          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.LESS_EQUAL, "<=", (), lineNumber) :: collectedTokens)
+          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "<", (), lineNumber) :: collectedTokens)
+        case '>' =>
+          if (isMatchNextChar('=')) scanTokens(currentIndex + 2, lineNumber, Token(TokenType.GREATER_EQUAL, ">=", (), lineNumber) :: collectedTokens)
+          else scanTokens(currentIndex + 1, lineNumber, Token(TokenType.EQUAL, "=", (), lineNumber) :: collectedTokens)
+        case '/' => scanTokens(currentIndex + 1, lineNumber, Token(TokenType.SLASH, "/", (), lineNumber) :: collectedTokens)
+        case _ => Scalox.error(lineNumber, "Unexpected string")
       }
     }
+
+    scanTokens(0, 0, Nil);
   }
 }
